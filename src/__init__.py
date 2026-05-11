9ab6dcc4047c091eb786455a09e9a22451d03428
Thomas Tilak <thomas.tilak@gmail.com>
2026-05-11 11:49:39 +1100

    updated package path

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,3 @@
 
 __version__ = "1.0.0"
 __author__ = "CKAN MCP Contributors"
-
-from .mcp_ckan_server import CKANAPIClient, handle_call_tool
-
-__all__ = [
-    "CKANAPIClient",
-    "handle_call_tool",
-]
